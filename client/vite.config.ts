d688c0656548ac68e53bc522ee137cdedce39faa
yuzi <19961257205@163.com>
2026-03-18 00:00:47 +0800

    feat: configure server settings in Vite for development environment

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -6,6 +6,10 @@ import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    host: "0.0.0.0",
+    port: 5173,
+  },
   plugins: [
     react(),
     tailwindcss(),
